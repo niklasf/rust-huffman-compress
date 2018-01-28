9526e5fae57311a56be8a0aea4c2754cc7dd2b1e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-01-28 21:42:02 +0100

    start implementing decoder

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,33 @@
 extern crate bit_vec;
 
 use std::borrow::Borrow;
-use std::hash::Hash;
 use std::collections::HashMap;
 use std::collections::BinaryHeap;
+use std::error::Error;
+use std::hash::Hash;
+use std::fmt;
 
 use bit_vec::BitVec;
 
 pub struct Tree<K> {
     root: usize,
     arena: Vec<Node<K>>,
+}
+
+impl<'a, K: Eq + Hash + Clone + 'a> Tree<K> {
+    pub fn decoder<I>(&'a self, iterable: I) -> Decoder<'a, I, K>
+        where I: IntoIterator<Item=bool>
+    {
+        Decoder {
+            tree: self,
+            iter: iterable.into_iter(),
+        }
+    }
+}
+
+pub struct Decoder<'a, I: IntoIterator<Item=bool>, K: 'a> {
+    tree: &'a Tree<K>,
+    iter: I::IntoIter,
 }
 
 pub struct Book<K> {
@@ -25,6 +43,15 @@ impl<K: Eq + Hash + Clone> Book<K> {
         where K: Borrow<Q>, Q: Hash + Eq
     {
         self.book.get(k)
+    }
+
+    pub fn encode<Q>(&self, buffer: &mut BitVec, k: &Q) -> Result<(), EncodeError>
+        where K: Borrow<Q>, Q: Hash + Eq
+    {
+        match self.book.get(k) {
+            Some(code) => Ok(buffer.extend(code)),
+            None => Err(EncodeError { }),
+        }
     }
 
     fn with_capacity(num_symbols: usize) -> Book<K> {
@@ -51,6 +78,21 @@ impl<K: Eq + Hash + Clone> Book<K> {
     }
 }
 
+#[derive(Debug)]
+pub struct EncodeError;
+
+impl fmt::Display for EncodeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        "encode error".fmt(f)
+    }
+}
+
+impl Error for EncodeError {
+    fn description(&self) -> &str {
+        "encode error"
+    }
+}
+
 struct Node<K> {
     weight: u64,
     parent: Option<usize>,
@@ -68,7 +110,7 @@ struct HeapData {
     id: usize,
 }
 
-pub fn codebook<K: Eq + Hash + Clone>(weights: HashMap<K, u64>) -> (Tree<K>, Book<K>) {
+pub fn codebook<K: Eq + Hash + Clone>(weights: &HashMap<K, u64>) -> (Tree<K>, Book<K>) {
     let num_symbols = weights.len();
 
     let mut heap = BinaryHeap::new();
@@ -79,16 +121,16 @@ pub fn codebook<K: Eq + Hash + Clone>(weights: HashMap<K, u64>) -> (Tree<K>, Boo
         let id = arena.len();
 
         heap.push(HeapData {
-            weight,
+            weight: *weight,
             id,
         });
 
         arena.push(Node {
-            weight,
+            weight: *weight,
             //id,
             parent: None,
             data: NodeData::Leaf {
-                symbol
+                symbol: symbol.clone()
             }
         });
     }
@@ -129,4 +171,28 @@ pub fn codebook<K: Eq + Hash + Clone>(weights: HashMap<K, u64>) -> (Tree<K>, Boo
     };
 
     (tree, book)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_uniform() {
+        let mut sample = HashMap::new();
+        sample.insert(1, 1);
+        sample.insert(2, 1);
+        sample.insert(3, 1);
+        sample.insert(4, 1);
+        sample.insert(5, 1);
+
+        let (tree, book) = codebook(&sample);
+
+        let mut buffer = BitVec::new();
+        book.encode(&mut buffer, &1).unwrap();
+        book.encode(&mut buffer, &2).unwrap();
+        book.encode(&mut buffer, &3).unwrap();
+        book.encode(&mut buffer, &4).unwrap();
+        book.encode(&mut buffer, &5).unwrap();
+    }
 }
