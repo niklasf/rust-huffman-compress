2aa05f5db8573864be308e50cb6fb18da787d630
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-01-29 00:31:34 +0100

    write module level doc

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,49 @@
 //! # Examples
 //!
 //! ```rust
+//! extern crate bit_vec;
+//! extern crate huffman_compress;
 //!
+//! # use std::error::Error;
+//! #
+//! # fn try_main() -> Result<(), Box<Error>> {
+//! use std::collections::HashMap;
+//! use bit_vec::BitVec;
+//! use huffman_compress::{ Book, Tree, codebook };
+//!
+//! let mut weights = HashMap::new();
+//! weights.insert("CG", 293);
+//! weights.insert("AG", 34);
+//! weights.insert("AT", 4);
+//! weights.insert("CT", 4);
+//! weights.insert("TG", 1);
+//!
+//! // Construct a Huffman code based on the weights (e.g. counts or relative
+//! // frequencies).
+//! let (book, tree) = codebook(&weights);
+//!
+//! println!("{:?}", book);
+//!
+//! // More frequent symbols will be encoded with fewer bits.
+//! assert!(book.get("CG").map_or(0, |cg| cg.len()) <
+//!         book.get("TG").map_or(0, |ag| ag.len()));
+//!
+//! // Encode some symbols using the book.
+//! let mut buffer = BitVec::new();
+//! let example = vec!["AT", "CG", "AT", "TG", "AG", "CT", "CT", "AG", "CG"];
+//! for symbol in &example {
+//!     book.encode(&mut buffer, symbol);
+//! }
+//!
+//! // Decode the symbols using the tree.
+//! let decoded: Vec<&str> = tree.decoder(&buffer).collect();
+//! assert_eq!(decoded, example);
+//! #     Ok(())
+//! # }
+//! #
+//! # fn main() {
+//! #     try_main().unwrap();
+//! # }
 //! ```
 
 #![doc(html_root_url = "https://docs.rs/huffman-compress/0.1.0")]
