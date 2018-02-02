43a562d4a7d9788a09cd4b066143d42a4ceac03a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-02-03 00:50:53 +0100

    benchmark the example from the readme

---
+++ benches/benches.rs
@@ -0,0 +1,37 @@
+#[macro_use]
+extern crate bencher;
+extern crate bit_vec;
+extern crate huffman_compress;
+
+use std::collections::HashMap;
+use bencher::{Bencher, black_box};
+use bit_vec::BitVec;
+use huffman_compress::codebook;
+
+fn bench_encode_decode(b: &mut Bencher) {
+    let mut weights = HashMap::new();
+    weights.insert("CG", 293);
+    weights.insert("AG", 34);
+    weights.insert("AT", 4);
+    weights.insert("CT", 4);
+    weights.insert("TG", 1);
+
+    let (book, tree) = codebook(&weights);
+
+    let example = black_box(vec!["AT", "CG", "AT", "TG", "AG", "CT", "CT", "AG", "CG"]);
+
+    b.iter(|| {
+        let mut buffer = BitVec::new();
+        for symbol in &example {
+            book.encode(&mut buffer, symbol).unwrap();
+        }
+
+        let decoded: Vec<&str> = tree.decoder(&buffer).collect();
+        assert_eq!(decoded, example);
+    });
+}
+
+benchmark_group!(benches,
+    bench_encode_decode);
+
+benchmark_main!(benches);
